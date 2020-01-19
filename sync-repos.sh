7652c074c91a02bf0563e5671428360f93960b0e
rajesh <rajesh.nitc@gmail.com>
2020-01-19 16:10:02 +0530

    first commit

--- sync-repos.sh
+++ sync-repos.sh
@@ -15,12 +15,12 @@ function syncRepos(){
 # jq compact is important for looping
 # removes whitespace and each element is put on a single line:
 # see the output of jq -c '.[]' repos.json, each element on single line:
-# {"projectName":"pipeline-helper","gitUrl":"https://pipeline-helper.git"}
-# {"projectName":"pipeline-orchestrator","gitUrl":"https://orchestrator.git"}
+# {"projectName":"repo1","gitUrl":"https://repo1.git"}
+# {"projectName":"repo2","gitUrl":"https://repo2.git"}
 
 # without compact option, there will be whitespace
 # and each element in the array will span over multiple lines
-# making it difiicult to treat it as single item while looping
+# making it difficult to treat it as single item while looping
 
 for i in $(jq -c '.[]' repos.json); do
  name=$(echo $i | jq '.projectName')
