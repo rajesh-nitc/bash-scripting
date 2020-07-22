e021d4b0984fa6bb49691aeb1886b4346ab418c7
Rajesh Gupta <rajesh.nitc@gmail.com>
2020-07-22 16:01:04 +0530

    get latest release from github

---
+++ get_latest_release_github.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl -sLO https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
