b51b011fc83f916fde05ad7f13679b02b4520954
Rajesh Gupta <rajesh.nitc@gmail.com>
2021-02-05 21:28:25 +0530

    Update setup_chromebook.sh

--- setup_chromebook.sh
+++ setup_chromebook.sh
@@ -19,7 +19,7 @@ sudo apt-get install -y code
 echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
 sudo apt-get update
-sudo apt-get install -y google-cloud-sdk
+sudo apt-get install -y google-cloud-sdk kubectl
 
 # terraform
 curl -k -O https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip
