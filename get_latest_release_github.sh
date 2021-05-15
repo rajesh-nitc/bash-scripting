eb8248ca3db445db44c90bc7388fe04d3a70a3ba
rajesh <rajesh.nitc@gmail.com>
2021-05-15 12:52:04 +0530

    removed some scripts

--- get_latest_release_github.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-curl -sLO https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
