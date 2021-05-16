15ff4a2b81120c57e0076c83b36c8179f046b812
Rajesh Gupta <rajesh.nitc@gmail.com>
2021-05-16 12:44:37 +0530

    Update setup_chromebook.sh

--- setup_chromebook.sh
+++ setup_chromebook.sh
@@ -67,3 +67,4 @@ curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o 
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo usermod -a -G docker ${USER}
